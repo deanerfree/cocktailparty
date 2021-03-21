0c9b0fde94f66af5d88bbaf7ce7d0baa13fb5638
Kurt Ball <kurtisdball@hotmail.com>
2021-03-20 21:52:57 -0600

    server location fixed

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Navbar = () => {
   return (
     <div className="navbar">
-      <div className="logo">Drinkapp</div>
+      <div className="logo">Cocktail Party</div>
       <div className="Topics"></div>
     </div>
   )
