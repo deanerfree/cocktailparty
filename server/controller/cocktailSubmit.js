54dacd31b87586f4e418cf54c0f00184d882703f
Kurt Ball <kurtisdball@hotmail.com>
2021-04-25 19:52:33 -0600

    get and post done

--- server/controller/cocktailSubmit.js
+++ server/controller/cocktailSubmit.js
@@ -1,12 +1,26 @@
 const CocktailModel = require('../Models/cocktailSchema')
-// const express = require('express')
-// const router = express.Router()
 
-exports.getCocktails = (req, res) => {
-  res.send('made it this far!')
-}
-exports.createCocktails = (req, res) => {
-  res.send('Gotit')
+//Requests a list of cocktails
+exports.getCocktails = async (req, res) => {
+  try {
+    const listOfCocktails = await CocktailModel.find()
+    res.status(200).json(listOfCocktails)
+  } catch (err) {
+    res.status(404).json({ message: err.message })
+  }
 }
 
-// module.exports = router
+//Creates a new cocktail in the library based on the CocktailModel
+exports.createCocktails = async (req, res) => {
+  const body = req.body
+
+  const newCocktail = new CocktailModel(body)
+  try {
+    //Need to check for duplicates in the library. Cannot have more than one cocktail of the same name
+    await newCocktail.save()
+
+    res.status(201).json(newCocktail)
+  } catch (err) {
+    res.status(404).json({ message: err.message })
+  }
+}
